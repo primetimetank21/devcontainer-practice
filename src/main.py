10026aa1b23937dd1cebde705f3c70abedbd5bda
TanK <theflashtank21@gmail.com>
2025-10-13 22:33:33 -0400

    feat: added last_logged_in to redis db     - refactor: updated Dockerfile name to `Dockerfile.dev`     - build: changed `fail-fast` in Python Versions to `false` so all builds run completely

--- src/main.py
+++ src/main.py
@@ -1,13 +1,34 @@
+from datetime import datetime, timezone
+from typing import Final
 from fastapi import FastAPI
+from redis.typing import ResponseT
 import redis
 
 app = FastAPI()
-r = redis.Redis(host="redis-db", port=6379)
+r = redis.Redis(host="redis-db", port=6379, decode_responses=True)
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    LAST_LOGIN_KEY: Final[str] = "last_login"
+    CURRENT_LOGIN_KEY: Final[str] = "current_login"
+
+    login_time: str = datetime.now(tz=timezone.utc).strftime("%d_%B_%Y_at_%H:%M:%S")
+
+    last_logged_in: ResponseT = r.get(LAST_LOGIN_KEY)
+    print(f"{last_logged_in=}")
+    print(f"{last_logged_in=}")
+
+    root_response: dict[str, str | ResponseT] = {"Hello": "World"}
+
+    if last_logged_in:
+        root_response[LAST_LOGIN_KEY] = last_logged_in
+
+    r.set(name=LAST_LOGIN_KEY, value=login_time)
+
+    root_response[CURRENT_LOGIN_KEY] = login_time
+
+    return root_response
 
 
 @app.get("/hits")
